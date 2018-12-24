cc5604e8ad7f8b79674afde7359a9520c4ee0057
Andy Goryachev <andy@goryachev.com>
2018-12-24 08:01:12 -0800

    .project

---
+++ src/goryachev/bugs/JavaBugs.java
@@ -0,0 +1,15 @@
+// Copyright © 2018 Andy Goryachev <andy@goryachev.com>
+package goryachev.bugs;
+
+
+/**
+ * Java Bugs.
+ */
+public class JavaBugs
+{
+	public static void main(String[] args)
+	{
+		System.out.println("OK");
+		System.exit(0);
+	}
+}
